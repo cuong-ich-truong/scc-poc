5c0070d6e063b90699f0f89b1e09e886fc281414
tran-doan <trandoan92ag@gmail.com>
2023-07-27 16:02:05 +0700

    #26, #27: Add popup to display alerts list; Show guards list to send alert; Add cypress test and update github actions to run e2e cypress test; Correct apis interface and response data model; Update env variable info in README

---
+++ frontend/src/types/Premise.ts
@@ -0,0 +1,20 @@
+export interface Premise {
+  id: string;
+  name: string;
+  cameras: Camera[];
+}
+
+export interface Camera {
+  id: string;
+  name: string;
+  streamUrl: string;
+  incidents: Incident[];
+}
+
+export interface Incident {
+  id: string;
+  message: string;
+  ignore: boolean;
+  guardId?: string;
+  dateCreated: string;
+}
