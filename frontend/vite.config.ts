da306075130c7aaaf67e4ce76a60a934ab05fe77
cuong-ich-truong <cuongichtruong@gmail.com>
2023-07-31 14:15:49 +0700

    #41: fix base path.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: `${env.VITE_BASE_PATH_PREFIX ?? '/ssc-poc/'}`,
+    base: `${env.VITE_BASE_PATH_PREFIX ?? '/scc-poc/'}`,
     test: {
       globals: true,
       environment: 'jsdom',
