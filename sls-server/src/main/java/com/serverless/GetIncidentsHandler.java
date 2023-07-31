f87ed154cd4be8e47db9933c73089f2cadd4297e
huynguyen-zuhlke <140476764+huynguyen-zuhlke@users.noreply.github.com>
2023-07-31 15:38:45 +0700

    5: CRUD for Incident and Note (#52)
    Co-authored-by: cuong-ich-truong <55036683+cuong-ich-truong@users.noreply.github.com>

---
+++ sls-server/src/main/java/com/serverless/GetIncidentsHandler.java
@@ -0,0 +1,40 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.dal.Incident;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class GetIncidentsHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+    private Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        try {
+            // get all premises
+            List<Incident> incidents = new Incident().list();
+
+            // send the response back
+            return ApiGatewayResponse.builder()
+                .setStatusCode(200)
+                .setObjectBody(incidents)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                .build();
+        } catch (Exception ex) {
+            logger.error("Error in listing incidents: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in listing incidents: ", input);
+            return ApiGatewayResponse.builder()
+                .setStatusCode(500)
+                .setObjectBody(responseBody)
+                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                .build();
+        }
+    }
+}
