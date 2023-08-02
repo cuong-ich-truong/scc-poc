fec813293e651dff1ef7e5881fdfb69a907fbf0c
tran-doan <trandoan92ag@gmail.com>
2023-08-02 13:22:12 +0700

    #38: Enable cors

--- sls-server/src/main/java/com/serverless/GetPremisesWithRelationsHandler.java
+++ sls-server/src/main/java/com/serverless/GetPremisesWithRelationsHandler.java
@@ -12,32 +12,32 @@ import java.util.Map;
 
 public class GetPremisesWithRelationsHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private final Logger logger = LogManager.getLogger(this.getClass());
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-    try {
-        // get all premises
-        List<Premise> premises = new Premise().listWithRelations();
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        try {
+            // get all premises
+            List<Premise> premises = new Premise().listWithRelations();
 
-        // send the response back
-        return ApiGatewayResponse.builder()
-    				.setStatusCode(200)
-    				.setObjectBody(premises)
-    				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
-						.enableCors()
-    				.build();
-    } catch (Exception ex) {
-        logger.error("Error in listing premises: " + ex);
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(premises)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .enableCors()
+                    .build();
+        } catch (Exception ex) {
+            logger.error("Error in listing premises: " + ex);
 
-        // send the error response back
-  			Response responseBody = new Response("Error in listing premises: ", input);
-  			return ApiGatewayResponse.builder()
-  					.setStatusCode(500)
-  					.setObjectBody(responseBody)
-  					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
-						.enableCors()	
-  					.build();
+            // send the error response back
+            Response responseBody = new Response("Error in listing premises: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .enableCors()
+                    .build();
+        }
     }
-	}
 }
