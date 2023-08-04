bcd53d50938885798838150b56241a0eb6a358bf
huynguyen-zuhlke <140476764+huynguyen-zuhlke@users.noreply.github.com>
2023-08-04 12:58:24 +0700

    Add unit tests (#101)
    * Add more unit tests
    
    * add more tests for premise
    
    * Remove backend and update README

--- backend/web/src/main/java/com/zuhlke/scc/web/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.zuhlke.scc.web.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
